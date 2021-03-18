2dcb52ab28baa0b001de6eef8c449087c1935109
Chris Wasser <cawasser@mac.com>
2021-03-18 10:35:55 -0400

    - added some comment-docs for request format, and set the stage for scrollable grids

--- src/cljs/alloc_ui/component/super_simple_grid.cljs
+++ src/cljs/alloc_ui/component/super_simple_grid.cljs
@@ -2,29 +2,29 @@
 
 
 (defn gen-id
-      ([ch ts]
-       (str ch "-" ts))
-      ([[ch ts]]
-       (str ch "-" ts)))
+  ([ch ts]
+   (str ch "-" ts))
+  ([[ch ts]]
+   (str ch "-" ts)))
 
 
-; TODO figure out how to draw the table from the sparse grid
 (defn allocation-grid [grid color-match]
-      (let [w 5 h 5]
-           [:table.is-hoverable
-            [:thead
-             [:tr [:th ""]
-              (for [x (range w)]
-                   ^{:key x} [:th.has-text-centered x])]]
-            [:tbody
-             (for [y (range h)]
-                  ^{:key y}
-                  [:tr [:th (str y)]
-                   (for [x (range w)]
-                     (let [t (first (get-in grid [(gen-id x y) :allocated-to]))]
-                       (if (nil? t)
-                         ^{:key (str x "-" 0)} [:td ""]
-                         ^{:key (str x "-" t)} [:td.has-text-centered
-                                                {:style {:background-color (first (get color-match t))
-                                                         :color (second (get color-match t))}}
-                                                (str t)])))])]]))
+  ; TODO: make the grid whatever size it needs to be
+  (let [w 5 h 5]
+    [:table.table
+     [:thead
+      [:tr [:th ""]
+       (for [x (range w)]
+         ^{:key x} [:th.has-text-centered x])]]
+     [:tbody
+      (for [y (range h)]
+        ^{:key y}
+        [:tr [:th (str y)]
+         (for [x (range w)]
+           (let [t (first (get-in grid [(gen-id x y) :allocated-to]))]
+             (if (nil? t)
+               ^{:key (str x "-" 0)} [:td ""]
+               ^{:key (str x "-" t)} [:td.has-text-centered
+                                      {:style {:background-color (first (get color-match t))
+                                               :color            (second (get color-match t))}}
+                                      (str t)])))])]]))
--- src/cljs/alloc_ui/dev_db.cljs
+++ src/cljs/alloc_ui/dev_db.cljs
@@ -7,6 +7,31 @@
 
 
 
+; How do "requests" work? What is the format for a valid request?
+
+; request is a set of pairs, channel followed by time-slot.
+;   for example: "a" would like channel 0 at time-slot 0:
+;          #{[0 0]}
+;
+;   "b" would like channel 1 at time-slot 1 and channel 2 at timeslot 2:
+;          #{[1 1] [2 2]}
+;
+; some flexibility is available (and some is planned but not yet implemented)
+;
+; (WORKING)
+; A requester might be able to one one of a small set of channel at a time-slot,
+; for example "a" can use either channel 2 OR 3 at time-slot 0:
+;          #{[[2 3] 0]}
+;
+; "b" can use either 3 OR 4 at slot 1 AND slots 1, 2 OR 3 at slot 3:
+;          #{[3 4] 1] [[1 2 3] 3]}
+;
+; (NOT working)
+; A requester might be flexible as to time, but NOT channel: "b" needs channel 2 at
+; either slot 1 OR 2:
+;          #{[2 [1 2]]}
+
+
 (def requests {"j" #{[0 0] [[1 2 3] 1]}
                "k" #{[0 2] [3 0]}
                "l" #{[[2 3] 0] [[2 3] 1] [[2 3] 2]}
